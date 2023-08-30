6e1d3932b49a1547eb38c540b9f12ccc7a515095
seojoon <iamaloneinseoul@gmail.com>
2023-08-30 16:21:23 +0900

    useState 사용/ onClick prop 추가, 상태업데이트/ 삼항조건연산자 사용해 표시여부 정의

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/* 경고 상자 만들기 */
 
-function App() {
+import React, { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  function deleteHandler() {
+    setIsDeleting(true);
+  }
+
+  function proceedHandler() {
+    setIsDeleting(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isDeleting ? (
+        <div id="alert">
+          <h2>Are you sure?</h2>
+          <p>These changes can't be reverted!</p>
+          <button onClick={proceedHandler}>Proceed</button>{" "}
+        </div>
+      ) : (
+        ""
+      )}
+
+      <button onClick={deleteHandler}>Delete</button>
     </div>
   );
 }
-
-export default App;
